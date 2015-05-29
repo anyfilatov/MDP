4c6efc7d232cce452389850581cb6433e6f7938c
Mikhail Bazhenov <rnby.mike@gmail.com>
2015-05-29 08:01:37 +0300

    fix test program

--- OrderedData/ClientLib/main.cpp
+++ OrderedData/ClientLib/main.cpp
@@ -91,40 +91,25 @@ int main(int argc, char *argv[])
     Client client(1, "settings.json");
     qDebug() << "===Test 1===";
     try{
-//        qDebug() << "INSERT:" << client.put("testInsert", "testValue");
+        client.put("testInsert", "testValue");
         qDebug() << "SEARCH:" << client.get("testInsert");
-//        qDebug() << "REMOVE:" << client.remove("testInsert");
+        client.remove("testInsert");
         qDebug() << "SEARCH:" << client.get("testInsert");
-//        qDebug() << "INSERT:" << client.put("testInsert", "testValue");
+        client.put("testInsert", "testValue");
         qDebug() << "SEARCH:" << client.get("testInsert");
-//        qDebug() << "REMOVE:" << client.remove("testInsert");
-        qDebug() << "SEARCH:" << client.get("testInsert");
-
-        qDebug() << "===Test 2===";
-        QList<QString> list;
-        list << "test1" << "test2" << "test3";
-//        qDebug() << "INSERT:" << client.put("testInsert", list);
-        qDebug() << "SEARCH:" << client.get("testInsert");
-        list << "test 4" << "test 5";
-//        qDebug() << "REPLACE:" << client.replace("testInsert", list);
-        qDebug() << "SEARCH:" << client.get("testInsert");
-//        qDebug() << "REMOVE:" << client.remove("testInsert");
+        client.remove("testInsert");
         qDebug() << "SEARCH:" << client.get("testInsert");
 
-        qDebug() << "===Test 3===";
-//        qDebug() << "INSERT BUCKET:" << client.put("testInsert1", "testValue", "TestBucket");
+//        qDebug() << "===Test 3===";
+//        client.put("testInsert1", "testValue", "TestBucket");
 //        qDebug() << "SEARCH BUCKET:" << client.getBucketKeys("TestBucket");
-        qDebug() << "SEARCH:" << client.get("testInsert1","TestBucket");
-//        qDebug() << "REMOVE:" << client.remove("testInsert1", "TestBucket");
+//        qDebug() << "SEARCH:" << client.get("testInsert1","TestBucket");
+//        client.remove("testInsert1", "TestBucket");
 
-        qDebug() << "===Test 4===";
-//        qDebug() << "INSERT BUCKET:" << client.put("testInsert2", list, "TestBucket");
+//        qDebug() << "===Test 4===";
 //        qDebug() << "SEARCH BUCKET:" << client.getBucketKeys("TestBucket");
-        qDebug() << "SEARCH:" << client.get("testInsert2","TestBucket");
-        list.clear();
-        list << "test6" << "test7";
-//        qDebug() << "REPLACE:" << client.replace("testInsert2", list, "TestBucket");
-        qDebug() << "SEARCH:" << client.get("testInsert2","TestBucket");
+//        qDebug() << "SEARCH:" << client.get("testInsert2","TestBucket");
+//        qDebug() << "SEARCH:" << client.get("testInsert2","TestBucket");
 
 
     } catch(ClientExeption& e){
